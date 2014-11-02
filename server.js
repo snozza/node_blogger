88454ec95f519f1bc066e6e10de402ec0ddb6f9d
Andrew Snead <andrewcsnead@gmail.com>
2014-11-02 20:39:18 +0000

    now renders 404 on wrong page

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 var http = require('http');
+var url = require('url');
+
+function makeNewPost(request, response) {
+  response.writeHead(200, {'Content-type': 'text/plain'});
+  response.end('Node Blogger - Make a new post');
+}
+
+function error404(request, response) { 
+  response.writeHead(404);
+  response.end('Whoops....404 - page not found');
+}
+
 
 var server = http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-type': 'text/plain'});
-  response.end('Node Blogger')
+  var postFormPattern = /^\/posts\/new\/?$/
+  var pathname = url.parse(request.url).pathname;
+  if (postFormPattern.test(pathname)) {
+    makeNewPost(request, response);
+  } else {
+    error404(request, response);
+  }
 });
 
 server.listen(9292);
